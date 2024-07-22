7096af5b348468ee60e0c4281c818ee512637e03
hexstories <kwakuasihene@gmail.com>
2024-07-22 15:07:25 +0000

    php configs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,16 +81,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Kwaku Asihene Essah 
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://www.linkedin.com/in/kwaku-essah">LinkedIn</a>
+                    <a href="https://x.com/hexstories_">twitter</a>
+                    <a href="mailto:kwakuasihene@gmail.com">Mail</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
